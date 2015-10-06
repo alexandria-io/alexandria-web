6b9dab38d24052042f9e49e40bf2f36ccca92593
Roerick Sweeney <sweeney@roerick.me>
2015-10-05 22:38:54 -0600

    so UM, this is theoretically a paywall.

--- src/app/js/paywall.js
+++ src/app/js/paywall.js
@@ -1,7 +1,16 @@
 function payWall() {
-    var txId = 67a0164b7ad1ca0c87e837445cec887bab32873c4f90bede6a03a49c83191f09
-        artistAddress = ['media-data']['alexandria-media']['info']['extra-info']['Bitcoin Address'];
+	//hard coding txId for now, needs some kind of input
+    var txId = 67a0164b7ad1ca0c87e837445cec887bab32873c4f90bede6a03a49c83191f09i;
+    var artistAddress = ['media-data']['alexandria-media']['info']['extra-info']['Bitcoin Address'];
+    var pwywData = ['media-data']['alexandria-media']['info']['extra-info']['pwyw'];
+    var minimumUnlockFee
+    var dispAddress = ['input-address']; 
+    var artifactCostBTC 
+    var artifactCostUSD
+    var BTCUSD // this value comes from main.js
+    var amountReceived 
 
+    //access libraryd
     $.ajax({
         type: 'POST',
         url: 'http://' + serverAddress + ':41289/alexandria/v1/search',
@@ -11,9 +20,32 @@ function payWall() {
             'search-for': txId
         },
         success: function(data) {
-            if (data['Bitcoin Address'])
+            //retreive artistAddress 
+	    if (data['Bitcoin Address'])
                 artistAddress = data['Bitcoin Address'];
+	    //retreive artifactCostUSD 
+	    if (data['pwyw'])
+		pwywData = data['pwyw']
+		//convert cost to btc
+		artifactCostUSD = // the second value in pwywData
+		artifactCostBTC = ((artifactCostUSD * 100) / BTCUSD) 
         }
     })
-        .get('https://blockchain.info/api/receive?method=create&address=' + artistAddress);
+    //create disposable address for the artist
+    .get('https://blockchain.info/api/receive?method=create&address=' + artistAddress, function ( data ) {
+	if (data['input_address'])
+	    dispAddress = data['input-address']	
+loadQR(dispAddress)    
+//check blockchain for transactions  , if no transactions, check again
+while (amountReceived < artifactCostBtc) 
+    .get('https://blockchain.info/q/getreceivedbyaddress/ + dispAddress {
+	if (data[])
+    	    amountReceived = data[]
+
+//load artifact
+if (amountReceived >= artifactCostBTC)
+  // load artifact , i don't know how to do this
+
 }
+
+
